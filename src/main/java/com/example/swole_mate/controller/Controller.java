8d48c8acc9ff085d5daaf194311957bbac7c38f1
Ruhan116 <capablecoder01@gmail.com>
2024-04-17 16:00:04 +0600

    small changes

--- src/main/java/com/example/swole_mate/controller/Controller.java
+++ src/main/java/com/example/swole_mate/controller/Controller.java
@@ -1,9 +1,9 @@
-package com.example.swole_mate;
+package com.example.swole_mate.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module com.example.swole_mate {
 
     opens com.example.swole_mate to javafx.fxml;
     exports com.example.swole_mate;
+    exports com.example.swole_mate.controller;
+    opens com.example.swole_mate.controller to javafx.fxml;
 }
